d6326c608a8d8bd9f3933e31a2dde3a2be13b7a6
MiraProfissional <jvitor.gmira@gmail.com>
2023-11-28 11:50:53 -0300

    Criação da rota de criação de inscrições + rota mi para extração dos dados do usuário + bd de inscrições

---
+++ model/Inscricao.js
@@ -0,0 +1,17 @@
+class Inscricao {
+
+    constructor(id, nome, idade, cidade, curso, redeSocial, celular, sobre, curiosidade, motivoEscolha) {
+        this.id = id;
+        this.nome = nome;
+        this.idade = idade;
+        this.cidade = cidade;
+        this.curso = curso;
+        this.redeSocial = redeSocial;
+        this.celular = celular;
+        this.sobre = sobre;
+        this.curiosidade = curiosidade;
+        this.motivoEscolha = motivoEscolha;
+    }
+  }
+  
+  module.exports = Inscricao;
