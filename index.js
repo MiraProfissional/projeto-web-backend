b72010a5f96f9283f314d706d0db79cee37cd40b
MiraProfissional <jvitor.gmira@gmail.com>
2023-11-29 16:52:02 -0300

    criação da rota de buscar uma republica pelo id + verifica Token na rota de criação de inscrição

--- index.js
+++ index.js
@@ -126,8 +126,25 @@ app.get('/republica/:nome', verificaToken, (req,res) => {
 
 })
 
+// Função para retornar uma reública com base no seu id
+app.get('/republica/:id', verificaToken, (req,res) => {
+
+    const jsonPath = path.join(__dirname, '.', 'db', 'banco-dados-republicas.json');
+    const republicas = JSON.parse(fs.readFileSync(jsonPath, { encoding: 'utf8', flag: 'r' }));
+
+    const params = req.params;    
+    //buscar republica
+    for(let republica of republicas){
+        if(params.id===republica.id){
+            return res.json(republica);
+        }
+    }
+    return res.status(403).send(`Nome Não Encontrada!`);
+
+})
+
 // Função para criar uma inscrição
-app.post('/create-inscricao', async (req,res) => {
+app.post('/create-inscricao',verificaToken, async (req,res) => {
     //extraindo os dados do formulário para criacao da inscrição
     const {nome, idade, cidade, curso, redeSocial, celular, sobre, curiosidade, motivoEscolha,republicaId,username} = req.body; 
     
--- model/User.js
+++ model/User.js
@@ -5,6 +5,7 @@ class User {
       this.username = username;
       this.email = email;
       this.password = password;
+      this.inscricoes = []
   }
 }
 
